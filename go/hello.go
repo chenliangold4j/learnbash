15906a11b51ab6ff893f8054b20f2970bde126e8
Your Name <you@example.com>
2020-12-18 14:42:32 +0800

    fix

--- go/hello.go
+++ go/hello.go
@@ -1,37 +1,22 @@
 package main
 
-import (
-    "os"
-    "fmt"
-    "io"
-    "strings"
-    )
-     
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // 将和送入 c
+}
+
 func main() {
-    // 打开原始文件
-    file, err := os.Open("../utils/daemon.json")
-    if err != nil {
-        fmt.Println("open file err :", err)
-        return
-    }
-    defer file.Close()
-    // 定义接收文件读取的字节数组
-    var buf [128]byte
-    var content []byte
-    for {
-        n, err := file.Read(buf[:])
-        if err == io.EOF {
-            // 读取结束
-            break
-        }
-        if err != nil {
-            fmt.Println("read file err ", err)
-            return
-        }
-        content = append(content, buf[:n]...)
-    }
-    
-    str := string(content);
-    str = strings.Replace(str,"#{自己的ip最后尾数}","12",-1);
-    fmt.Println(str)
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // 从 c 中接收
+
+	fmt.Println(x, y, x+y)
 }
