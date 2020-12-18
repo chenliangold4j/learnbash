c226f48b000b623ec33af45118a334552baa0a19
Your Name <you@example.com>
2020-12-18 18:00:35 +0800

    fix

--- go/mydocker/dockex2.go
+++ go/mydocker/dockex2.go
@@ -13,8 +13,8 @@ func main(){
 		Cloneflags: syscall.CLONE_NEWUTS
 		| syscall.CLONE_NEWIPC 
 		| syscall.CLONE_NEWPID 
-		| syscall.CLONE_NEWNS
-		| syscall.CLONE_NEWUSER,
+		| syscall.CLONE_NEWNS,
+		// | syscall.CLONE_NEWUSER,
 	}   
 		cmd.SysProcAttr.Credential = 
 		&syscall.Credential{Uid:uint32(1),Gid:uint32(1)}
