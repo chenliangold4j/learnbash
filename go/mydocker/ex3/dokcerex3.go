f4a87e87dbaeceffe0e099ba919e64d196b9e100
Your Name <you@example.com>
2020-12-23 15:33:42 +0800

    ex3

--- go/mydocker/ex3/dokcerex3.go
+++ go/mydocker/ex3/dokcerex3.go
@@ -2,13 +2,32 @@ package main
 
 import(
 	"os/exec"
+	"path"
+	"fmt"
+	"io/ioutil"
+	"strconv"
 	"syscall"
 	"os"
-	"log"
 )
 
+const cgroupMemoryHierarchMount = "/sys/fs/cgroup/memory"
+
 func main(){
-	cmd :=exec.Command("sh")
+	if(os.Args[0] == "/proc/self/exe"){
+		//容器进程
+		fmt.Printf("current pid %d",syscall.Getpid())
+		fmt.Println()
+		cmd := exec.Command("sh","-c",`stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr= &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err:=cmd.Run();err != nil{
+			fmt.Println(err)
+		}
+		os.Exit(1)
+	}
+	cmd := exec.Command("/proc/self/exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
 		UidMappings: []syscall.SysProcIDMap{
@@ -28,13 +47,20 @@ func main(){
 	}  
 	// 在我的版本  go1.15.6 不能有这行，，1.7 可以
 		// cmd.SysProcAttr.Credential = &syscall.Credential{Uid:uint32(1),Gid:uint32(1)}
-		cmd.Stdin = os.Stdin 
-		cmd.Stdout= os.Stdout 
-		cmd.Stderr = os . Stderr
+	cmd.Stdin = os.Stdin 
+	cmd.Stdout= os.Stdout 
+	cmd.Stderr = os . Stderr
 
 	if err := cmd.Run();err != nil{
-		log.Fatal(err)
-	}
-	os.Exit(-1)
+		fmt.Println("ERROR",err)
+		os.Exit(1)
+	}else{
+		fmt.Printf("%v",cmd.Process.Pid)
 
+		os.Mkdir(path.Join(cgroupMemoryHierarchMount,"testmemorylimit"),0755)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchMount,"testmemorylimit","task"),[]byte(strconv.Itoa(cmd.Process.Pid)),0644)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchMount,"testmemorylimit","memory.limit_in_bytes"),[]byte("100m"),0644)
+
+	}
+	cmd.Process.Wait()
 }
